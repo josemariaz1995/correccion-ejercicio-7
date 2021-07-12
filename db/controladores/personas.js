c432c375d912eab8b088180d6fc42653b695d4e0
Sandra Cuéllar <sandra.cllp.22@gmail.com>
2021-07-12 17:52:22 +0200

    ruta persona vacunada

--- db/controladores/personas.js
+++ db/controladores/personas.js
@@ -1,5 +1,31 @@
 const Persona = require("../modelos/Persona");
 
+const listarPersonaPorDni = async (dni) => {
+  const persona = await Persona.find()
+    .where("dni")
+    .eq(dni.toUpperCase());
+  return persona;
+};
+const modificarPersonaVacunada = async (id, modificaciones) => {
+  try {
+    const modificarPersona = await Persona.findByIdAndUpdate(
+      id,
+      modificaciones
+    );
+    const personaModificada = await Persona.findById(id);
+    return personaModificada;
+  } catch (error) {
+    console.log(`Error al modificar persona: ${error.message}`);
+  }
+};
+const eliminarRegistroPersona = async (id) => {
+  try {
+    const peronsalEliminada = await Persona.findByIdAndDelete(id);
+    return peronsalEliminada;
+  } catch (error) {
+    console.log(`Error al eliminar registro ${error.message}`);
+  }
+};
 const crearPersonaVacunada = async (
   dni,
   idCentro,
@@ -24,4 +50,7 @@ const crearPersonaVacunada = async (
 
 module.exports = {
   crearPersonaVacunada,
+  eliminarRegistroPersona,
+  modificarPersonaVacunada,
+  listarPersonaPorDni,
 };
